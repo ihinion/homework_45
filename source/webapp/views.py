a2034a33eb340fc3fd1d072b1c386e3fd68e9e8f
ilia <jamiashvili@gmail.com>
2020-07-21 11:02:11 +0600

    Created index.html, index_view, url path for index

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from webapp.models import Task
 
-# Create your views here.
+
+def index_view(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'index.html', context)
