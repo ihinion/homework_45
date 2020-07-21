c9b719b58bf18c32b09a86ccdab0eef04e958345
ilia <jamiashvili@gmail.com>
2020-07-21 15:33:39 +0600

    Modified index.html and index_view for correct display of status, fixed minor error in models.py

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from webapp.models import Task
+from django.shortcuts import redirect
+from webapp.models import STATUS_CHOICES
 
 
 def index_view(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+    options = []
+    for i in STATUS_CHOICES:
+        option = {i[0]: i[1]}
+        options.append(option)
+    context = {'tasks': tasks, 'options': options}
     return render(request, 'index.html', context)
