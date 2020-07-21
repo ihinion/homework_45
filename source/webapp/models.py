fe47692b078ff31f93f2fe6a4c8afc5d82778b5e
ilia <jamiashvili@gmail.com>
2020-07-21 10:47:44 +0600

    Created model Task, registered Task in admin.py, migrated webapp

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('new', 'New'), ('in_progress', 'In progress'), ('done', 'Done')]
+
+class Task(models.Model):
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name='Description')
+    status = models.TextField(max_length=15, verbose_name='Status', default=STATUS_CHOICES[0][0],
+                              choices=STATUS_CHOICES)
+    finish_date = models.DateField(max_length=10, null=True, blank=True, default='', verbose_name='Finished')
+
+    def __str__(self):
+        return f'{self.pk}. {self.title}'
